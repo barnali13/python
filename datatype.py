8ac556c30aa9c4c8883cb0286df48e3162a534fc
Barnali Kalita <kalitabarnali57@gmail.com>
2026-01-07 15:35:47 +0530

    practiced concepts of python

---
+++ datatype.py
@@ -0,0 +1,43 @@
+x = "Hello"
+print(x)
+print(type(x))
+
+x = 50
+print(x)
+print(type(x))
+
+x = 20.5
+print(x)
+print(type(x))
+
+x = 2+3j
+print(x)
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x))
+
+x = ("apple", "banana", "cherry")
+print(x)
+print(type(x))
+
+
+x = range(6)
+print(x)
+print(type(x))
+
+x = {"name" : "Barnali", "age" : 21}
+print(x)
+print(type(x))
+
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print(type(x))
+
+x = True
+
+print(x)
+
+print(type(x)) 
+
---
+++ syn.py
@@ -0,0 +1,16 @@
+#print("Hello, World!")
+#This is comment
+#if 5>2:
+    #print("Five is greater than two!")
+
+# that variable names are case-sensitive
+#Variable
+x = 10
+Y = "Hello"
+print(x)
+print(Y)
+#Global Variables
+x = "Hello"
+def myfunc():
+    print("python is " + x)
+myfunc()
