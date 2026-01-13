95503d13d1145ede7957b8c783c67dcdc9584d42
Barnali Kalita <kalitabarnali57@gmail.com>
2026-01-13 10:36:28 +0530

    mutable vs Immutable

---
+++ mutable.py
@@ -0,0 +1,24 @@
+#Mutable objects can be modified in place without creating a new object.
+#Immutable objects cannot be changed after creation.
+
+# list = [1,2,3,4]
+# list.append(5) 
+# print(list) 
+# list.extend([7,8])
+# print(list)
+# list.insert(5,6)
+# print(list)
+
+
+# list = [1,2,3,4]
+# pop = list.pop(0)
+# print(pop)
+
+# my_dict = {"name": "Ram", "age": 25}
+# new_dict = my_dict
+# new_dict["age"] = 37
+# print(new_dict)
+
+x = 10
+x = x +5 
+print(x)
