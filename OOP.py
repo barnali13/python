a3c4b89104a4f1e77b6b0450b164aa2cf359fcc1
Barnali Kalita <kalitabarnali57@gmail.com>
2026-01-19 17:13:47 +0530

    problem solved

--- OOP.py
+++ OOP.py
@@ -50,3 +50,33 @@
 # print("Name:", emp2.name)
 # print("Emp ID:", emp2.emp_id)
 # print("Email:", emp2.email)
+
+# #Question:Create a class Employee with attributes: emp_id  name departmen
+
+# class Employee:
+#     def __init__(self,emp_id,name,department):
+#         self.emp_id = emp_id
+#         self.name = name
+#         self.department = department
+# emp1 = Employee(101, "Rahul", "IT")
+# emp2 = Employee(102, "Raj", "HR")
+
+# print("emp1 details:")
+# print("Emp ID:", emp1.emp_id)
+# print("Name:", emp1.name)
+# print("Department:", emp1.department)
+
+# print("emp2 details:")
+# print("Emp ID:", emp2.emp_id)
+# print("Name:", emp2.name)
+# print("Department:", emp2.department)
+
+#Question:Create a class UserLogin with: email p Use __init__ to initialize these values.
+
+class UserLogin:
+    def __init__(self,email,p):
+        self.email = email
+        self.p = p
+
+user = UserLogin("admin@example.com", "admin123")
+print(f"Email: {user.email}, P: {user.p} logged in successfully")
