b2a48d2a9777bde83edf0feaa03f8e51be32ad88
Barnali Kalita <kalitabarnali57@gmail.com>
2026-01-19 07:18:30 +0530

    oop  problem

---
+++ OOP.py
@@ -0,0 +1,52 @@
+# class Employee:
+#     def __init__(self, emp_id, name):
+#         self.emp_id = emp_id
+#         self.name = name
+
+# emp1 = Employee(101, "Rahul")
+
+
+# class Employee:
+#     def __init__(self, emp_id, name):
+#         self.emp_id = emp_id
+#         self.name = name
+
+# emp1 = Employee(101, "Rahul")
+
+# print("id = ", emp1.emp_id)
+# print("name = ", emp1.name)
+
+
+# class Employee:
+#     def __init__(self, emp_id, name, age=18):  #Default values 
+#         self.emp_id = emp_id
+#         self.name = name
+#         self.age = age
+
+# emp1 = Employee(101, "Rahul", 25)
+# emp2 = Employee(102, "Raj")
+
+# print("id = ", emp1.emp_id)
+# print("name = ", emp1.name)
+# print("age = ", emp1.age)
+# print("id = ", emp2.emp_id)
+# print("name = ", emp2.name)
+# print("age = ", emp2.age)
+
+# class Person:
+#     def __init__(self,name,emp_id,email):
+#         self.name = name 
+#         self.emp_id = emp_id
+#         self.email = email
+# emp1 = Person("Rahul", 101, "rahul@example.com")
+# emp2 = Person("Raj", 102, "raj@example.com")
+
+# print("emp1 details:")
+# print("Name:", emp1.name)
+# print("Emp ID:", emp1.emp_id)
+# print("Email:", emp1.email)
+
+# print("emp2 details:")
+# print("Name:", emp2.name)
+# print("Emp ID:", emp2.emp_id)
+# print("Email:", emp2.email)
